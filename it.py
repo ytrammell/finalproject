23a8a359147498505f5dc892d76deb90f133a596
ytrammell <ytrammell@wisc.edu>
2017-12-02 11:54:58 -0600

    yes

---
+++ it.py
@@ -0,0 +1,20 @@
+'''
+Created on Dec 2, 2017
+
+@author: ITAUser
+'''
+numberlist = [];
+"""
+numberlist.append(3);
+numberlist.append(6);
+numberlist.append(7);
+
+print (numberlist);
+"""
+
+for i in range(10):
+    if (i%3 !=0): """doesnt equal"""
+    numberlist.append(i);
+    if (10%i ==0):
+        numberlist.append(i);
+print (numberlist);
